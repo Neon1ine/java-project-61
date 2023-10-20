a3e587fcc2a5916e5b607c332b12a438b72e3ee4
Neon1ine <azeemin1548@yandex.ru>
2023-10-20 20:40:27 +0300

    rework even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,7 +14,14 @@ public class App {
                     break;
                 case 2:
                     username = Cli.greeting();
-                    startEvenGame(username);
+                    System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+                    startGame(2 ,username);
+                    isRunning = false;
+                    break;
+                case 3:
+                    username = Cli.greeting();
+                    System.out.println("What is the result of the expression?");
+                    startGame(3, username);
                     isRunning = false;
                     break;
                 case 0:
@@ -31,22 +38,24 @@ public class App {
         System.out.print("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
+                + "3 - Calc\n"
                 + "0 - Exit\n"
                 + "Your choice: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 
-    private static void startEvenGame(String username) {
-        final int levelsToBeatTheGame = 3;
-        final int lastLevel = 0;
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+    private final static int LEVELS_TO_BEAT_THE_GAME = 3;
+    private static final int RANDOM_GENERATOR_RANGE = 100;
+
+    private static void startGame(int gameId, String username) {
         boolean isCorrect = true;
-        int level = levelsToBeatTheGame;
+        int level = LEVELS_TO_BEAT_THE_GAME;
         while (isCorrect) {
             --level;
-            isCorrect = oneRoundEvenGame(level);
-            if (level == lastLevel && isCorrect) {
+            isCorrect = oneRoundOfGame(gameId, level);
+            if (level == 0 && isCorrect) {
+                //todo stringBuilder
                 System.out.println("Congratulations " + username + "!");
                 break;
             }
@@ -54,21 +63,59 @@ public class App {
 
     }
 
-    private static boolean oneRoundEvenGame(int level) {
-        int number = (int) (Math.random() * 100);
-        System.out.println("Question: " + number);
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Your answer: ");
-        String answer = scanner.nextLine();
-        String correctAnswer = (number % 2 == 0) ? "yes" : "no";
-        if (answer.equals(correctAnswer)) {
+    private static boolean oneRoundOfGame(int gameId, int level) {
+        String question = defineQuestion(gameId);
+        String userAnswer = askUserForAnswer(question);
+        String correctAnswer = defineCorrectAnswer(gameId, question);
+        if (userAnswer.equals(correctAnswer)) {
             if (level != 1) {
                 System.out.println("Correct!");
             }
             return true;
         } else {
-            System.out.println("'" + answer + "'  is wrong answer ;(. Correct answer was " + correctAnswer);
+            //todo stringBuilder
+            System.out.println("'" + userAnswer + "'  is wrong answer ;(. Correct answer was '" + correctAnswer +"'");
             return false;
         }
     }
+
+    private static String defineQuestion(int gameId) {
+        switch (gameId) {
+            case 2:
+                return generateRandomNumber();
+            case 3:
+                return generateRandomNumber() + " + " + generateRandomNumber();
+            default:
+                System.out.println("Error in defineQuestion: wrong gameId - " + gameId);
+                return null;
+
+        }
+    }
+
+    private static String generateRandomNumber() {
+        return String.valueOf((int) (Math.random() * RANDOM_GENERATOR_RANGE));
+    }
+
+    private static String askUserForAnswer(String question) {
+        System.out.println("Question: " + question);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your answer: ");
+        return scanner.nextLine();
+    }
+
+    private static String defineCorrectAnswer(int gameId, String question) {
+        switch (gameId) {
+            case 2:
+                return (Integer.parseInt(question) % 2 == 0) ? "yes" : "no";
+            case 3:
+                //todo разбить вопрос на 2 числа и знак
+                return null;
+            default:
+                System.out.println("Error in defineCorrectAnswer: wrong gameId - " + gameId);
+                return null;
+        }
+
+    }
+
+
 }
